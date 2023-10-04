95879b69d752408983c8595fd321999853ddf9f8
deepy <45151004+deepytux@users.noreply.github.com>
2023-10-03 20:19:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 
 function discord() {
-    navigator.clipboard.writeText("neeko_iko")
-    titlec("copied to clipboard")
-  }
+    navigator.clipboard.writeText("deepy_")
+=  }
